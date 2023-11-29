0ba7aaf0a86fd44fe686963c1a24165045ef37e5
Mallikarjun0535 <mitta.mallik@gmail.com>
2023-11-29 16:30:24 +0530

    First Basic code to develop

---
+++ twitterkafka/src/main/java/com/kafka/practise/twitterkafka/TwitterkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.practise.twitterkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterkafkaApplication.class, args);
+	}
+
+}
---
+++ twitterkafka/src/test/java/com/kafka/practise/twitterkafka/TwitterkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.practise.twitterkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
