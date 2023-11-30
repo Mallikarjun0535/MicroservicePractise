d183c5220871fd0c94adbd87d397202f5385f291
Mallikarjun0535 <mitta.mallik@gmail.com>
2023-11-30 18:07:23 +0530

    basic twitter kafka is running with mock server

---
+++ twitterkafka/src/main/java/com/kafka/practise/twitterkafka/TwitterToKafkaServiceException.java
@@ -0,0 +1,16 @@
+package com.kafka.practise.twitterkafka;
+
+public class TwitterToKafkaServiceException extends RuntimeException {
+
+    public TwitterToKafkaServiceException() {
+        super();
+    }
+
+    public TwitterToKafkaServiceException(String message) {
+        super(message);
+    }
+
+    public TwitterToKafkaServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
