e8bee24e87e44a3c9b7836106899781b717216b0
Mallikarjun0535 <mitta.mallik@gmail.com>
2023-11-30 20:23:09 +0530

    changing to muti maven

--- twitterkafka/src/main/java/com/kafka/practise/twitterkafka/KafkaConfig.java
+++
@@ -1,7 +0,0 @@
-package com.kafka.practise.twitterkafka;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class KafkaConfig {
-}
--- twitterkafka/src/main/java/com/kafka/practise/twitterkafka/runner/StreamRunner.java
+++
@@ -1,9 +0,0 @@
-package com.kafka.practise.twitterkafka.runner;
-
-import twitter4j.TwitterException;
-
-public interface StreamRunner {
-
-    void start() throws TwitterException;
-
-}
